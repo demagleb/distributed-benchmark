11c0ddbc60fef77e58d1b5676c35a818e607f5e3
eugenedar <evgenedar@mail.ru>
2023-06-05 18:09:32 +0300

    stable version

--- src/main/client/client.c
+++ src/main/client/client.c
@@ -9,7 +9,7 @@
 enum { BUFFER_SIZE = 1024, MAX_ATTEMPTS = 100 };
 
 int send_fileinfo(int socket, char *file_name, size_t file_size) {
-    FileInfo file_info;
+    struct FileInfo file_info;
     file_info.file_size = file_size;
     memset(file_info.file_name, 0, sizeof(file_info.file_name));
     strcpy(file_info.file_name, file_name);
