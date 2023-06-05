e25133307a2f7487fe94356b77f80d02d730dcc2
nastya_biran <anast.biran@gmail.com>
2023-06-05 20:15:57 +0300

    add nonblocking write and fix reading fileinfo

---
+++ src/main/common/fileinfo.h
@@ -0,0 +1,5 @@
+#pragma once
+struct FileInfo {
+    size_t file_size;
+    char file_name[FILENAME_MAX + 1];
+};
--- src/main/server/server.c
+++ src/main/server/server.c
@@ -20,7 +20,6 @@
 #include "worker_handler.h"
 
 
-int client = 0;
 
 
 
@@ -28,7 +27,8 @@ int client = 0;
 
 int main(int argc, char *argv[]) {
     struct Worker workers[MAX_FDS];
-    struct FILE_EXE file = {.content = NULL, .size = 0, .read_size = -1};
+    struct Client client = {.file.content = NULL};
+    remove_client(&client);
     signal(SIGPIPE, SIG_IGN);
     if (argc != 4) {
         fprintf(stderr, "Usage: %s SERVICE\n", argv[0]);
@@ -66,21 +66,26 @@ int main(int argc, char *argv[]) {
         }
         if (evt.data.fd == client_sock) {
             accept_client(evt, &epollfd, &client);
-            printf("%d\n", client);
         }
         if (evt.data.fd != sock && evt.data.fd != client_sock) {
-            printf("%d\n", evt.data.fd);
-            if (evt.data.fd == client) {
-                get_file_from_client(&client, &file);
-                if (file.size > 0 && file.size == file.read_size) {
-                    hire_workers(workers, file.content, file.size);
+            if (evt.data.fd == client.fd) {
+                get_file_from_client(&client);
+                if (client.file.info.file_size > 0 && client.file.info.file_size == client.file.read_size) {
+                    printf("Got full file. Start to pass jobs to workers\n");
+                    hire_workers(workers, client.file.content, client.file.info.file_size);
                 }
             } else {
-                if (workers[evt.data.fd].status == CONNECTED) {
-                    get_worker_params(workers, &evt);
-                }
-                if (workers[evt.data.fd].status == IS_WORKING) {
-                    get_results(workers, evt, &client);
+                if (evt.events & EPOLLOUT) {
+                    if (workers[evt.data.fd].status == READY_FOR_TASK) {
+                        continue_to_write_file_to_worker(workers, evt.data.fd, client.file.content, client.file.info.file_size);
+                    }
+                } else {
+                    if (workers[evt.data.fd].status == CONNECTED) {
+                        get_worker_params(workers, &evt);
+                    }
+                    if (workers[evt.data.fd].status == IS_WORKING) {
+                        get_results(workers, evt, &client);
+                    }
                 }
             }
 
